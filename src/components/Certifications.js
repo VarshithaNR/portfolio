c2451eb11d4bfff06957d30491bc1f72952016b7
VarshithaNR <rakshuvarshu216@gmail.com>
2025-06-01 20:51:10 +0530

    Update Certifications.js

---
+++ src/components/Certifications.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Certifications = () => (
+  <section>
+    <h2>Certifications</h2>
+    <ul>
+      <li>Web Development – IUCEE EWB MCE Student Chapter (2022)</li>
+      <li>C Programming – Great Learning Academy (2024)</li>
+      <li>Ethical Hacking – NPTEL (2024)</li>
+    </ul>
+  </section>
+);
+
+export default Certifications;
